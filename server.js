4d65c1229b8f8c1c6326476552414edf29f28d93
TalesC <tales.pereira@gympass.com>
2020-08-10 13:29:27 -0300

    add mongodb, orm and nodemon

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//Inicinado o App
 const app = express();
+
+//Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+//Primeira Rota
+app.get('/', (req, res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+    
+    return res.send('Hello World!');
+});
 
 app.listen(3001);
